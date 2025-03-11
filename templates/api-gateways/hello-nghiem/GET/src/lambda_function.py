fe5df22002bb7b13fe4f01f06d649f153a9c99ac
nghiemxuan <nghiemxuan2006@gmail.com>
2025-03-11 17:51:55 +0700

    Add AWS Lambda function and CloudFormation templates for API Gateway and Lambda Layers with dependencies

---
+++ templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
@@ -0,0 +1,6 @@
+def lambda_handler(event, context):
+    # Process the incoming event and return a response
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Xuan nghiem!'
+    }
