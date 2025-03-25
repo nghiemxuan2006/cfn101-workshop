c775643b69d7fc858174b510869f4137f64c4b4e
nghiemxuan <nghiemxuan2006@gmail.com>
2025-03-25 23:49:02 +0700

    Add logging for event in lambda function and enhance request template for age validation

--- templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
+++ templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
@@ -1,6 +1,7 @@
 import pandas as pd
 def handler(event, context):
     # Process the incoming event and return a response
+    print("event: ", event)
     return {
         'statusCode': 200,
         'body': 'Hello from Xuan nghiem thongminh!'
