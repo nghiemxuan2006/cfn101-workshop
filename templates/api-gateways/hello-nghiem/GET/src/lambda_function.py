03641a1bbacc89e27559fe16a52c10a5f4309957
nghiemxuan <nghiemxuan2006@gmail.com>
2025-03-26 17:41:24 +0700

    Add context logging to handler in lambda_function for improved debugging

--- templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
+++ templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
@@ -2,6 +2,7 @@ import pandas as pd
 def handler(event, context):
     # Process the incoming event and return a response
     print("event: ", event)
+    print("context: ", context)
     name = event["name"]
     person = {
         "name": name,
