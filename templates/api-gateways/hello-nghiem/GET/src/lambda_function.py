6ce8b99814458dda12cbe120ab54b56c21212600
nghiemxuan <nghiemxuan2006@gmail.com>
2025-03-26 17:40:59 +0700

    Refactor handler in lambda_function to return person object with name and optional age; update template.yml for API Gateway response model

--- templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
+++ templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
@@ -2,7 +2,11 @@ import pandas as pd
 def handler(event, context):
     # Process the incoming event and return a response
     print("event: ", event)
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Xuan nghiem thongminh!'
+    name = event["name"]
+    person = {
+        "name": name,
     }
+    if "age" in event:
+        person["age"] = event["age"]
+
+    return person
