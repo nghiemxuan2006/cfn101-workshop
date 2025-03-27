1a03c1a2467d288d357c19dd88ba562542d3d3b7
nghiemxuan <nghiemxuan2006@gmail.com>
2025-03-27 15:22:59 +0700

    Add CloudFormation templates and deployment script for API Gateway stacks

---
+++ templates/bash_scripts/prepare_templates.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+# Define variables
+STACK_NAME_PREFIX="nx-vid"
+API_GATEWAYS_DIR="templates/api-gateways"
+
+echo $COMMIT_HASH
+echo $BRANCH_NAME
+
+# Deploy API Gateway stacks
+if [ -d "$API_GATEWAYS_DIR" ]; then
+  echo "Deploying API Gateway stacks..."
+  
+  # Find all template.yml files recursively
+  find "$API_GATEWAYS_DIR" -type f -name "template.yml" | while read -r TEMPLATE_FILE; do
+    # Extract endpoint and method names from the directory structure
+    ENDPOINT_NAME=$(basename "$(dirname "$TEMPLATE_FILE")" | tr '[:upper:]' '[:lower:]')
+    METHOD_NAME=$(basename "$(dirname "$(dirname "$TEMPLATE_FILE")")" | tr '[:upper:]' '[:lower:]')
+
+    echo "Deploying API Gateway stack for endpoint '$ENDPOINT_NAME' and method '$METHOD_NAME'..."
+    echo $TEMPLATE_FILE
+    # Replace placeholder in the template file
+    sed -i "s/|COMMIT_HASH|/$COMMIT_HASH/g" "$TEMPLATE_FILE"
+
+    echo 
+    # Upload the template file to S3
+    # aws s3 cp "$TEMPLATE_FILE" "s3://$S3_BUCKET_NAME/template.yml"
+  done
+else
+  echo "API Gateways directory not found: $API_GATEWAYS_DIR"
+  exit 1
+fi
+
+echo "All API Gateway stacks deployed successfully."
