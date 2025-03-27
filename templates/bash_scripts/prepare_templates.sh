7bff2c7b322873c68b6c159ae68cfbabdde06709
nghiemxuan <nghiemxuan2006@gmail.com>
2025-03-27 15:58:46 +0700

    Fix script to correctly upload API Gateway templates to S3 and clean up commented code

--- templates/bash_scripts/prepare_templates.sh
+++ templates/bash_scripts/prepare_templates.sh
@@ -16,13 +16,15 @@ if [ -d "$API_GATEWAYS_DIR" ]; then
   # Find all template.yml files recursively
   find "$API_GATEWAYS_DIR" -type f -name "template.yml" | while read -r TEMPLATE_FILE; do
     # Extract endpoint and method names from the directory structure
-    ENDPOINT_NAME=$(basename "$(dirname "$TEMPLATE_FILE")" | tr '[:upper:]' '[:lower:]')
-    METHOD_NAME=$(basename "$(dirname "$(dirname "$TEMPLATE_FILE")")" | tr '[:upper:]' '[:lower:]')
+    # ENDPOINT_NAME=$(basename "$(dirname "$TEMPLATE_FILE")" | tr '[:upper:]' '[:lower:]')
+    # METHOD_NAME=$(basename "$(dirname "$(dirname "$TEMPLATE_FILE")")" | tr '[:upper:]' '[:lower:]')
 
-    echo "Deploying API Gateway stack for endpoint '$ENDPOINT_NAME' and method '$METHOD_NAME'..."
-    echo $TEMPLATE_FILE
+    # echo "Deploying API Gateway stack for endpoint '$ENDPOINT_NAME' and method '$METHOD_NAME'..."
+    # echo $TEMPLATE_FILE
     # Replace placeholder in the template file
     sed -i "s/|COMMIT_HASH|/$COMMIT_HASH/g" "$TEMPLATE_FILE"
+    FOLDER_PATH = $(dirname "$TEMPLATE_FILE")
+    echo $FOLDER_PATH
 
     # Upload the template file to S3
     # aws s3 cp "$TEMPLATE_FILE" "s3://$S3_BUCKET_NAME/$STACK_NAME_PREFIX/$TEMPLATE_FILE"
