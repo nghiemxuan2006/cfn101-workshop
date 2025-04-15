756428f017df588cb973307fb5ad7a4af4643f6a
nghiemxuan <nghiemxuan2006@gmail.com>
2025-04-15 16:06:41 +0700

    Update name value in person dictionary in lambda_function.py

--- templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
+++ templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
@@ -5,7 +5,7 @@ def handler(event, context):
     print("context: ", context)
     name = event["name"]
     person = {
-        "name": name
+        "name": "nghieemmmm"
     }
     if "age" in event:
         person["age"] = event["age"]
