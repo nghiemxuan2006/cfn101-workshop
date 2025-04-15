608bef6827d40213ef0971f978d4f5e18fa71237
nghiemxuan <nghiemxuan2006@gmail.com>
2025-04-15 18:09:39 +0700

    Fix typo in name value in person dictionary in lambda_function.py

--- templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
+++ templates/api-gateways/hello-nghiem/GET/src/lambda_function.py
@@ -5,7 +5,7 @@ def handler(event, context):
     print("context: ", context)
     name = event["name"]
     person = {
-        "name": "nghieemmmm"
+        "name": "nghieemmmmm"
     }
     if "age" in event:
         person["age"] = event["age"]
