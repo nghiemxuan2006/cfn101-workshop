5d2a3761b6af98b39dffccb6d7bf59034b266c3a
nghiemxuan <nghiemxuan2006@gmail.com>
2025-06-11 15:13:37 +0700

    update: modify out of memory testing code in main.py to allocate larger data

--- templates/fargate/fibonaci/src/main.py
+++ templates/fargate/fibonaci/src/main.py
@@ -14,14 +14,14 @@ def fibonacci(n):
 
 if __name__ == "__main__":
     # test ecs task container out of memory
-    # data = []
-    # for _ in range(5000):
-    #     data.append("X" * 10**6)  # 1MB mỗi phần tử
+    data = []
+    for _ in range(16):
+        data.append("X" * 10**9)  # 1GB mỗi phần tử
     # test lambda out of memory
     # try:
-    large_list = []
-    for i in range(5):
-        large_list.append('x' * 1024 * 1024 * 1024 * 16)
+        # large_list = []
+        # for i in range(5):
+        #     large_list.append('x' * 1024 * 1024 * 1024 * 16)
     # except MemoryError:
     #     print("MemoryError caught: Lambda function ran out of memory.")
     n = int(os.getenv("FIBONACCI_NUMBER", 2))  # Default to 2 if not provided
